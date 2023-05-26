f96e1c5fb84ba4839d310dc79f1397f1c9954ade
cristianurcuqui <cristianurcuqui@gmail.com>
2023-05-25 23:32:51 -0500

    Siguiente commit

--- main.go
+++ main.go
@@ -1,35 +1,37 @@
 package main
 
 import (
-	"go_basic/iteracciones"
+	"go_basic/ejercicio2"
 )
 
 func main() {
 	/*
-			estado, text := variables.ConviertoTexto(123)
-			fmt.Println(estado)
+				estado, text := variables.ConviertoTexto(123)
+				fmt.Println(estado)
+				fmt.Println(text)
+				if os := runtime.GOOS; os == "linux" || os == "mac" {
+					fmt.Println("El sistema OS es:", os)
+				} else {
+					fmt.Println("El sistema No es unix")
+				}
+
+				switch os := runtime.GOOS; os {
+				case "linux":
+					fmt.Println("El sistema es linux")
+				case "windows":
+					fmt.Println("El sistema darwin")
+				default:
+					fmt.Printf("%s \n", os)
+				}
+
+
+			numero, text := ejercicios.EjercicioUno("200")
+			fmt.Println(numero)
 			fmt.Println(text)
-			if os := runtime.GOOS; os == "linux" || os == "mac" {
-				fmt.Println("El sistema OS es:", os)
-			} else {
-				fmt.Println("El sistema No es unix")
-			}
-
-			switch os := runtime.GOOS; os {
-			case "linux":
-				fmt.Println("El sistema es linux")
-			case "windows":
-				fmt.Println("El sistema darwin")
-			default:
-				fmt.Printf("%s \n", os)
-			}
 
 
-		numero, text := ejercicios.EjercicioUno("200")
-		fmt.Println(numero)
-		fmt.Println(text)
+		iteracciones.Iterar()
 	*/
-
-	iteracciones.Iterar()
+	ejercicio2.Calculadora()
 
 }
