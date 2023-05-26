bc31907d17e033eac2ad3ca5ef7d5e4a7b676051
cristianurcuqui <cristianurcuqui@gmail.com>
2023-05-25 22:29:07 -0500

    Siguiente commit

---
+++ ejercicios/ejercicio01.go
@@ -0,0 +1,17 @@
+package ejercicios
+
+import (
+	"strconv"
+)
+
+func EjercicioUno(numero string) (int, string) {
+	text, err := strconv.Atoi(numero)
+	if err != nil {
+		return 0, "Invalid"
+	}
+	if text > 100 {
+		return text, numero
+	} else {
+		return text, "Es menor a 100"
+	}
+}
--- main.go
+++ main.go
@@ -2,11 +2,32 @@ package main
 
 import (
 	"fmt"
-	"go_basic/variables"
+	"go_basic/ejercicios"
 )
 
 func main() {
-	estado, text := variables.ConviertoTexto(123)
-	fmt.Println(estado)
+	/*
+		estado, text := variables.ConviertoTexto(123)
+		fmt.Println(estado)
+		fmt.Println(text)
+		if os := runtime.GOOS; os == "linux" || os == "mac" {
+			fmt.Println("El sistema OS es:", os)
+		} else {
+			fmt.Println("El sistema No es unix")
+		}
+
+		switch os := runtime.GOOS; os {
+		case "linux":
+			fmt.Println("El sistema es linux")
+		case "windows":
+			fmt.Println("El sistema darwin")
+		default:
+			fmt.Printf("%s \n", os)
+		}
+	*/
+
+	numero, text := ejercicios.EjercicioUno("200")
+	fmt.Println(numero)
 	fmt.Println(text)
+
 }
