62ea2b6a63113ca33823fde8e3e9f496bd7c9455
cristianurcuqui <cristianurcuqui@gmail.com>
2023-06-01 23:04:32 -0500

    Siguiente commit

--- main.go
+++ main.go
@@ -1,62 +1,57 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-	"time"
-)
-
 func main() {
 	/*
-				estado, text := variables.ConviertoTexto(123)
-				fmt.Println(estado)
+					estado, text := variables.ConviertoTexto(123)
+					fmt.Println(estado)
+					fmt.Println(text)
+					if os := runtime.GOOS; os == "linux" || os == "mac" {
+						fmt.Println("El sistema OS es:", os)
+					} else {
+						fmt.Println("El sistema No es unix")
+					}
+
+					switch os := runtime.GOOS; os {
+					case "linux":
+						fmt.Println("El sistema es linux")
+					case "windows":
+						fmt.Println("El sistema darwin")
+					default:
+						fmt.Printf("%s \n", os)
+					}
+
+
+				numero, text := ejercicios.EjercicioUno("200")
+				fmt.Println(numero)
 				fmt.Println(text)
-				if os := runtime.GOOS; os == "linux" || os == "mac" {
-					fmt.Println("El sistema OS es:", os)
-				} else {
-					fmt.Println("El sistema No es unix")
-				}
-
-				switch os := runtime.GOOS; os {
-				case "linux":
-					fmt.Println("El sistema es linux")
-				case "windows":
-					fmt.Println("El sistema darwin")
-				default:
-					fmt.Printf("%s \n", os)
-				}
 
 
-			numero, text := ejercicios.EjercicioUno("200")
-			fmt.Println(numero)
-			fmt.Println(text)
+			iteracciones.Iterar()
+			ejercicio2.Calculadora()
+			fmt.Println(ejercicio2.Calculadora())
 
 
-		iteracciones.Iterar()
-		ejercicio2.Calculadora()
-		fmt.Println(ejercicio2.Calculadora())
-	*/
+		numbers := []int{7, 2, 5, 6, 1, 3, 9, 8, 4}
+		fmt.Println("Antes de ordenar:", numbers)
 
-	numbers := []int{7, 2, 5, 6, 1, 3, 9, 8, 4}
-	fmt.Println("Antes de ordenar:", numbers)
+		sort.Slice(numbers, func(i, j int) bool {
+			return numbers[i] < numbers[j]
+		})
 
-	sort.Slice(numbers, func(i, j int) bool {
-		return numbers[i] < numbers[j]
-	})
+		fmt.Println("Después de ordenar:", numbers)
 
-	fmt.Println("Después de ordenar:", numbers)
-
-	LongCalculation(func(progress float64) {
-		fmt.Printf("El cálculo está %.2f%% completo\n", progress*100)
-	})
-}
-
-func LongCalculation(callback func(progress float64)) {
-	for i := 0; i <= 10; i++ {
-		time.Sleep(1 * time.Second) // Simula un cálculo que tarda un tiempo en completarse.
-
-		// Calcula el progreso como un porcentaje y llama al callback.
-		progress := float64(i) / 10.0
-		callback(progress)
+		LongCalculation(func(progress float64) {
+			fmt.Printf("El cálculo está %.2f%% completo\n", progress*100)
+		})
 	}
+
+	func LongCalculation(callback func(progress float64)) {
+		for i := 0; i <= 10; i++ {
+			time.Sleep(1 * time.Second) // Simula un cálculo que tarda un tiempo en completarse.
+
+			// Calcula el progreso como un porcentaje y llama al callback.
+			progress := float64(i) / 10.0
+			callback(progress)
+		}
+	*/
 }
